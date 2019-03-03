c9f2cadd6cda9a11fa286fa9bcd65baadcc272ac
Thomas Jager <thomasj@thomasjager.ca>
2019-03-02 19:09:52 -0500

    Uncommented

--- user/src/proc2.s
+++ user/src/proc2.s
@@ -33,40 +33,40 @@ path:
 
 .export proc2
 .proc proc2
-        ; pea     O_RDONLY
-        ; pea     path
-        ; jsr     open
-        ; rep     #$30
-        ; ply
-        ; ply
+        pea     O_RDONLY
+        pea     path
+        jsr     open
+        rep     #$30
+        ply
+        ply
 
-        ; pea     tmp
-        ; pea     0
-        ; pha
-        ; cop     9
-        ; rep     #$30
-        ; ply
-        ; ply
-        ; ply
+        pea     tmp
+        pea     0
+        pha
+        cop     9
+        rep     #$30
+        ply
+        ply
+        ply
 
-        ; pea     10
-        ; pea     tmp_str
-        ; pha
-        ; jsr     itoa
-        ; rep     #$30
-        ; ply
-        ; ply
-        ; ply
-        ; pea     tmp_str
-        ; jsr     puts
-        ; rep     #$30
-        ; ply
+        pea     10
+        pea     tmp_str
+        pha
+        jsr     itoa
+        rep     #$30
+        ply
+        ply
+        ply
+        pea     tmp_str
+        jsr     puts
+        rep     #$30
+        ply
 
 
-        ; pea     tmp + DirEnt::name
-        ; jsr     puts
-        ; rep     #$30
-        ; ply
+        pea     tmp + DirEnt::name
+        jsr     puts
+        rep     #$30
+        ply
 
 loop:
         jsr     getchar
